4b9e5ea315e974a8a2970afb113ae5f1d34d0e15
Kapil <bhattkapil.777@gmail.com>
2024-08-31 17:22:18 +0530

    static files serving for github

--- Backend/index.js
+++ Backend/index.js
@@ -1,36 +1,44 @@
-import express from "express";
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-import cors from "cors";
+import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import path from 'path'
 
-import bookRoute from "./route/book.route.js";
-import userRoute from "./route/user.route.js";
+import bookRoute from './route/book.route.js'
+import userRoute from './route/user.route.js'
 
-const app = express();
+const app = express()
 
-app.use(cors());
-app.use(express.json());
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '../Frontend/build')))
 
-dotenv.config();
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../Frontend/build', 'index.html'))
+})
 
-const PORT = process.env.PORT || 4000;
-const URI = process.env.MongoDBURI;
+app.use(cors())
+app.use(express.json())
+
+dotenv.config()
+
+const PORT = process.env.PORT || 4000
+const URI = process.env.MongoDBURI
 
 // connect to mongoDB
 try {
-    mongoose.connect(URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    });
-    console.log("Connected to mongoDB");
+  mongoose.connect(URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  console.log('Connected to mongoDB')
 } catch (error) {
-    console.log("Error: ", error);
+  console.log('Error: ', error)
 }
 
 // defining routes
-app.use("/book", bookRoute);
-app.use("/user", userRoute);
+app.use('/book', bookRoute)
+app.use('/user', userRoute)
 
 app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
-});
+  console.log(`Server is listening on port ${PORT}`)
+})
